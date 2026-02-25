cb88360d202baa6d37971ec5466edf3e46a56ba2
susana-278 <susana.ospinaj@upb.edu.co>
2026-02-25 09:26:38 -0500

    condicionales anidadas
    chain reaction

---
+++ 5.CondicionalesAnidadas/5.CondicionalesAnidadas/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.CondicionalesAnidadas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Diseñar un algoritmo que lea tres números A, B, C y visualice el valor más grande. El usuario debe ingresar tres valores diferentes.*/
+            int num1 = 0;
+            int num2 = 0;
+            int num3 = 0;
+
+            Console.WriteLine("Ingrese tres valores diferentes");
+
+            Console.WriteLine("Ingrese el valor del número 1");
+            num1= 0; Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor del número 2");
+            num2 = 0; Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor del número 3");
+            num3 = 0; Int32.Parse(Console.ReadLine());
+            /*if (num1>num2)
+            {
+                if (num1 > num3)
+                {
+                    Console.WriteLine($"El número mayor es:{num1}");
+                }
+                else
+                {
+                    Console.WriteLine($"El número mayor es:{num3}");
+                }
+
+            }
+            else
+            {
+                if (num2 > num3)
+                {
+                    Console.WriteLine($"El número mayor es:{num2}");
+                }
+                else
+                {
+                    Console.WriteLine($"El número mayor es: {num3}");
+                }
+            }*/
+            if (num1 > num2 && num1 > num3)
+            {
+                Console.WriteLine($"El número mayor es:{num1}");
+            }
+            else
+            {
+                if (num2 > num1 && num2 > num3)
+                {
+                    Console.WriteLine($"El número mayor es:{num2}");
+                }
+            }
+
+        }  
+    }
+}
+/*Realizar un algoritmo que permita solicitar 3 calificaciones, calcular el promedio.
+ * Si el promedio se ubica en los siguientes rangos, escribir los correspondientes mensajes. Considerar la situación en donde el promedio generado no esté
+ * contemplado en los rangos establecidos, mostrar un mensaje de error:
+ * 9.5 - 10 Excelente
+ * 8.5 - 9.4 Muy bien
+ * 7.5 - 8.5 Bien*/
+
+namespace _5.CondicionalesAnidadas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cal1 = 0;
+            int cal2 = 0;
+            int cal3 = 0;
+            Console.WriteLine("Ingrese la primera calificación");
+            cal1 = 0; Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la segunda calificación");
+            cal2 = 0; Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la tercera calificación");
+            cal3 = 0; Int32.Parse(Console.ReadLine());
+        }
+        
---
+++ 5.CondicionalesAnidadas/5.CondicionalesAnidadas/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("5.CondicionalesAnidadas")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("5.CondicionalesAnidadas")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("e17e2a7f-e59c-4059-96b5-7e80a8eb22d9")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
