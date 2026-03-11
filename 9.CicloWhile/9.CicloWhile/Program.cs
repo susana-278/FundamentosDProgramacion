e7b6c2be175a03405663a7d96d889680d3694670
susana-278 <susana.ospinaj@upb.edu.co>
2026-03-11 09:30:24 -0500

    Add files via upload

---
+++ 9.CicloWhile/9.CicloWhile/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9.CicloWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int contador = 1;
+            int acumulador = 0;
+
+            while (contador <= 5)
+            {
+                acumulador += contador;
+                contador++;
+
+                    Console.WriteLine($"Contador: {contador} - Acumulador = {acumulador}");
+         
+            }
+            Console.WriteLine($"La suma de los cinco primeros números enteros es: {acumulador}");*/
+
+            int contador = 1;
+            int acumulador = 1;
+            int numero = 0;
+
+            Console.WriteLine("Ingrese un número paraa calcular su factorial");
+            numero=Int32.Parse(Console.ReadLine());
+
+            while (contador <=numero)
+            {
+                acumulador *= contador;
+                contador++;
+
+            }
+            Console.WriteLine($"La factorial del número {numero} es: {acumulador}");
+        }
+
+    }
+}
